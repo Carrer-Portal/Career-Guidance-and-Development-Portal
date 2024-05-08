3378a640200455c13fae27230ccd71ccc8363b24
kpathir2 <kpathir2@wiley.com>
2024-05-08 11:40:38 +0530

    Initialize the backend and connect the Database

---
+++ Server/server.js
@@ -0,0 +1,27 @@
+const express =require('express');
+const mongoose = require('mongoose');
+const bodyParser = require( "body-parser");
+const cors = require ("cors");
+const dotenv = require ("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 8070;
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL)
+.then(()=>{
+        console.log("DB Connected");   
+        }
+    )
+.catch((error)=>
+        {
+            console.log("DB Connection error: " + error);   
+        }
+    )
+ app.listen(PORT,()=>{
+    console.log(`Server listening on port ${PORT}`);
+ });

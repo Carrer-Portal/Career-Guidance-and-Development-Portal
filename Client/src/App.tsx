b44ce52b9fc601d48935a0a332e87c3c5730e9ee
Dilan Induwara <153802063+Induwara04@users.noreply.github.com>
2024-12-05 15:34:10 +0530

    Style Change

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,14 +1,20 @@
 import React from "react";
+import {  Routes, Route } from "react-router-dom";
 import ProfilePage from "./pages/UserProfile/userprofile";
 import Login from "./pages/LogingPage/LoginPage";
-import SignUp from "./pages/SignUp/SignUp";
+import SignUp from "./pages/SignUp/signup";
+import "./main.css";
+import Home from "./pages/Home/home";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Login />
-      <SignUp />
-      <ProfilePage />
+    <div className="body">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   );
 };

224cd1e5ded978ad38f8adceca471e0a2c400c5f
Dilan Induwara <153802063+Induwara04@users.noreply.github.com>
2024-12-06 21:09:16 +0530

    add booking page

--- Client/src/Partials/Footer/Footer.tsx
+++ Client/src/Partials/Footer/Footer.tsx
@@ -6,18 +6,35 @@ const Footer: React.FC = () => {
   return (
     <Box className="footer" component="footer">
       <Box className="footer-links">
-        <Link href="/terms" className="footer-link" underline="none" color="none">
+        <Link
+          href="/terms"
+          className="footer-link"
+          underline="none"
+          color="none"
+        >
           Terms of service
         </Link>
-        <Link href="/privacy" className="footer-link" underline="none">
+        <Link
+          href="/privacy"
+          className="footer-link"
+          underline="none"
+          color="none"
+        >
           Privacy Policy
         </Link>
-        <Link href="/support" className="footer-link" underline="none">
+        <Link
+          href="/support"
+          className="footer-link"
+          underline="none"
+          color="none"
+        >
           Support
         </Link>
       </Box>
 
-      <Typography className="footer-copyright">© 2024, All rights reserved</Typography>
+      <Typography className="footer-copyright">
+        © 2024, All rights reserved
+      </Typography>
     </Box>
   );
 };
--- Client/src/pages/BookingPage/BookingPage.tsx
+++ Client/src/pages/BookingPage/BookingPage.tsx
@@ -13,21 +13,43 @@ import {
   StepLabel,
 } from "@mui/material";
 import "./BookingPage.css";
+import Chathu from "../../image/Chathu.jpeg";
+import Niroshani from "../../image/Niroshani.png";
+import madhuwanthi from "../../image/madhuwanthi.png";
 
 const BookingPage = () => {
   const [date, setDate] = useState(new Date());
   const [timeSlot, setTimeSlot] = useState("");
-  const timeSlots = ["9:30", "10:30", "11:30", "12:30"];
+  const [selectedAdvisor, setSelectedAdvisor] = useState("");
+  const timeSlots = ["9:30", "10:30", "11:30", "12:30", "1:30", "2:30", "3:30"];
 
   const handleTimeSlotClick = (slot: string) => {
     setTimeSlot(slot);
+    console.log(`Selected Time: ${slot}`);
+  };
+
+  const handleAdvisorSelect = (name: string) => {
+    setSelectedAdvisor(name);
+    console.log(`Selected Advisor: ${name}`);
   };
 
   return (
     <Box className="booking-container">
-      <Grid container spacing={4} >
+      <Box className="booking-Title">
+        <Typography variant="h5">Select Your Time and Book Them</Typography>
+        <Typography variant="body1" className="sub-Title">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </Typography>
+      </Box>
+      <Grid container spacing={4} md={12} lg={12}>
         <Grid item xs={12} md={6} className="left-section">
-          <Typography variant="h6" className="section-title">
+          <Typography
+            variant="h6"
+            className="section-title"
+            fontWeight={600}
+            mb={2}
+          >
             Choose a Date
           </Typography>
           <Calendar
@@ -35,7 +57,12 @@ const BookingPage = () => {
             onChange={(date) => setDate(date as Date)}
             className="date-picker"
           />
-          <Typography variant="h6" className="section-title">
+          <Typography
+            variant="h6"
+            className="section-title"
+            fontWeight={600}
+            mb={2}
+          >
             Pick a Time
           </Typography>
           <Box className="time-slots">
@@ -43,55 +70,82 @@ const BookingPage = () => {
               <Button
                 key={slot}
                 variant={timeSlot === slot ? "contained" : "outlined"}
-                color="primary"
+                color="secondary"
                 onClick={() => handleTimeSlotClick(slot)}
               >
                 {slot}
               </Button>
             ))}
           </Box>
-          <Typography variant="caption" className="timezone">
-            All Times are in Eastern Time - US & Canada
-          </Typography>
-          <Box className="action-buttons">
-            <Button variant="outlined">Back</Button>
-            <Button variant="contained" color="primary">
-              Continue
-            </Button>
-          </Box>
+          <Typography variant="caption" className="timezone"></Typography>
         </Grid>
 
-        {/* Right Section */}
         <Grid item xs={12} md={6} className="right-section">
-          <Box className="profile-container">
-            {[1, 2, 3].map((profile, index) => (
+          <Typography
+            variant="h6"
+            className="section-title"
+            fontWeight={600}
+            mb={2}
+          >
+            Select Your Advisor
+          </Typography>
+          <Box className="ad-profile-container">
+            {[
+              {
+                name: "chathurangani thennakoon",
+                image: Chathu,
+                designation: "Senior Career Advisor",
+              },
+              {
+                name: "shashika niroshani",
+                image: Niroshani,
+                designation: "Career Advisor",
+              },
+              {
+                name: "Nirosha Madhuwanthi.",
+                image: madhuwanthi,
+                designation: "Career Advisor",
+              },
+            ].map((profile, index) => (
               <Box key={index} className="profile-box">
-                <Avatar
-                  src="https://via.placeholder.com/100"
-                  alt={`Profile ${index + 1}`}
-                />
-                <Typography variant="body1" className="profile-name">
-                  Juliya Hue
-                </Typography>
+                <Box className="profile-content">
+                  <Avatar
+                    src={profile.image}
+                    alt={profile.name}
+                    className="profile"
+                    sx={{ width: 100, height: 100 }}
+                  />
+                  <Typography variant="body1" mt={2}>
+                    {profile.name}
+                  </Typography>
+                  <Typography variant="body2" mb={2}>
+                    {profile.designation}
+                  </Typography>
+                  <Button
+                    variant="outlined"
+                    color="secondary"
+                    onClick={() => handleAdvisorSelect(profile.name)}
+                  >
+                    Select
+                  </Button>
+                </Box>
               </Box>
             ))}
           </Box>
+          <Typography
+            variant="h6"
+            className="section-title"
+            fontWeight={600}
+            mt={5}
+          >
+            Fill Contact Details
+          </Typography>
+          <Typography variant="body2" mb={3}>
+            Please fill your information so we can get in touch with you.
+          </Typography>
           <Box className="form-container">
-            <Stepper activeStep={1}>
-              {[1, 2, 3, 4].map((step) => (
-                <Step key={step}>
-                  <StepLabel></StepLabel>
-                </Step>
-              ))}
-            </Stepper>
-            <Typography variant="h6" className="form-title">
-              Contact Details
-            </Typography>
-            <Typography variant="body2" className="form-description">
-              Please fill your information so we can get in touch with you.
-            </Typography>
             <Grid container spacing={2}>
-              <Grid item xs={12} md={6}>
+              <Grid item xs={12} md={12}>
                 <TextField label="Name" fullWidth />
               </Grid>
               <Grid item xs={12} md={6}>
@@ -100,13 +154,19 @@ const BookingPage = () => {
               <Grid item xs={12} md={6}>
                 <TextField label="Phone Number" fullWidth />
               </Grid>
-              <Grid item xs={12} md={6}>
-                <TextField label="Company" fullWidth />
+              <Grid item xs={12} md={12}>
+                <TextField label="Description" fullWidth />
               </Grid>
             </Grid>
           </Box>
         </Grid>
       </Grid>
+      <Box className="action-buttons">
+        <Button variant="outlined" color="secondary">Back</Button>
+        <Button variant="contained" color="secondary">
+          Continue
+        </Button>
+      </Box>
     </Box>
   );
 };
